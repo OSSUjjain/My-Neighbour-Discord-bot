4d4efc9f880dda02872e67b294bcbd8796a94140
VinitGurjar <vinitfagna@gmail.com>
2023-10-05 00:01:44 +0530

    Initial commit via vanilla js config

---
+++ index.js
@@ -0,0 +1,7 @@
+//Using discord.js lib for devekoping the bot using js, this lib revolves around events
+import { Client } from "discord.js";
+
+//client
+const client = new Client();
+
+//res to ready event
