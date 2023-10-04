a3e24ef1151485a7d5cb87f38b9eb6c106dd99b7
VinitGurjar <vinitfagna@gmail.com>
2023-10-05 02:11:21 +0530

    feat: Use discord.js lib to implement bots events

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+/*
 //Using discord.js lib for devekoping the bot using js, this lib revolves around events
 const Discord = require("discord.js");
 
@@ -18,3 +35,4 @@ client.on("message", (msg) => {
 
 //Have the client to log in to the server and we are going to use our bot token here but not in a string format rather via env
 client.login(process.env.TOKEN);
+*/
