e149a7cb4c8a187d305c8acd0ec7128df8549e80
VinitGurjar <vinitfagna@gmail.com>
2023-10-05 01:41:03 +0530

    refactor: move eslint and fix dependencies

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 //Using discord.js lib for devekoping the bot using js, this lib revolves around events
-import { Client } from "discord.js";
+const Discord = require("discord.js");
 
 //client
-const client = new Client();
+const client = new Discord.Client();
 
-//res to ready event
+//responed to ready event
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//reponed to message event
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+//Have the client to log in to the server and we are going to use our bot token here but not in a string format rather via env
+client.login(process.env.TOKEN);
