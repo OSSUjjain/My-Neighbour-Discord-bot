50667f94635baa838f3ea98d64ae3bf2250e554a
Vinit <vinitfagna@gmail.com>
2023-11-16 14:24:56 +0530

    fix!: Change from commonjs to es module
    issue1: Parsing error: 'import' and 'export' may appear only with 'sourceType: module'eslint
    solution: refer to previous commit #a189daa
    
    issue2-with-solution: instead of using this commonjs syntax:
    ```javascript
    const { Events, GatewayIntentBits } =  require("discord.js");
    ```
    USE es module syntax that is:
    ```javascript
    import { Events, GatewayIntentBits } from ("discord.js");
    ```
    
    issue3: Parsing error: Unexpected token `Events` eslint
    getting the above error on writing :
    ```javascript
    import Client, Events, GatewayIntentBits from 'discord.js' ;
    ```
    solution: Eslint treats 'Events' as a token/variable name rather than a named export.
    So the fix is to separate the **default and named imports**

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+//Import the necessary discord.js classes
+/*The Discord.js library has a default export (Client) as well as named 
+exports (Events, GatewayIntentBits) so import them separetly.
+
+Eslint treats 'Events' as a token/variable name rather than a named export.
+So the fix is to separate the **default and named imports**
+*/
+import Client from "discord.js";
+import { Events, GatewayIntentBits } from "discord.js";
+import token from "./config.json";
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
