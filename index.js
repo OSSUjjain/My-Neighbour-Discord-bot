955a904cd9dde59e26b954e1498d0afffac04d57
Vinit <vinitfagna@gmail.com>
2023-12-23 22:28:19 +0530

    feat: Receiving command interactions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Import the necessary discord.js classes
+
 /*The Discord.js library has a default export (Client) as well as named 
 exports (Events, GatewayIntentBits) so import them separetly.
 
@@ -6,8 +7,10 @@ Eslint treats 'Events' as a token/variable name rather than a named export.
 So the fix is to separate the **default and named imports**
 */
 import Client from "discord.js";
-import { Events, GatewayIntentBits } from "discord.js";
+import { Events, GatewayIntentBits, Collection } from "discord.js";
 import token from "./config.json";
+import fs from "node:fs";
+import path from "node:path";
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -20,3 +23,28 @@ client.once(Events.ClientReady, (c) => {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+Client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
+
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
+});
